1b3a1d5a289709362ab16681214113059389eba8
Jonatan <jonatan.lledo@gmail.com>
2020-05-25 11:26:13 +0200

    Primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/*jshint esversion: 6 */
+// -- REQUIRES -- //
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicializar variables
+var app = express();
+
+// conexión con la BBDD
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if ( err ) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');    
+
+});
+
+// -- ROUTES -- // IMPORTANT:
+app.get( '/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        message: 'Petición realizada correctamente'
+    });
+} );
+
+// Escuchar peticiones (puerto 3000)
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
